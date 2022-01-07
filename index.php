8a4fd2e54ae4ffa1a07109dc316b78ed450bcba0
hama0garde <79602965+hama0garde@users.noreply.github.com>
2022-01-07 19:53:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+   ob_start();
+   session_start();
+?>
+<html>
+  <body style=background-color:black;>
+    <div>
+      <center style=color:yellow;>
+      Hello World
+        </center>
+      </div>
+    </body>
+  </html>
